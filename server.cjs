a3ac0abf0b4ddd4c16dec9138904c91af34fd6a9
giordanoenzo <enzogiordanoofficial@gmail.com>
2025-12-31 13:30:11 -0300

    novo ajuste

--- server.cjs
+++ server.cjs
@@ -273,6 +273,6 @@ app.get(new RegExp('.*'), (req, res) => {
 });
 
 // Start Server
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server running on http://localhost:${port}`);
 });
